3e1f7c52140c793659760d22de010ebd959f6cab
Carlos <demirose41@gmail.com'git commitgit logpwdNsdad>
2019-09-08 22:45:53 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,16 @@
 console.log("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+const cards = ["queen","queen","king","king"];
+const cardsInPlay =[];
+var cardOne = cards[0];//storing first item in cards
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+var cardTwo = cards[1];//storing third item in cards
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}else{
+		alert("Sorry, try again.")
+	}
+}
